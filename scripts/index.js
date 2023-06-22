cabf85ed1bef4b20e14f9fea957d048759a041e4
Naol <37399395+Belayne@users.noreply.github.com>
2023-06-22 16:01:52 +0200

    Add basic calculator html and style

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,8 @@
 
+let firstNum;
+let secondNum;
+let operator;
+
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -8,6 +12,7 @@ const multiply = (a, b) => a * b;
 const power = (a, b) => a ** b;
 
 const divide = (a, b) => {
+    if(b == 0) return "Ayo you can't do that";
     let result = a / b;
     return formatDivide(result);
 }
@@ -15,4 +20,15 @@ const divide = (a, b) => {
 const formatDivide = (result) => {
     let formattedRes = (Number.isInteger(result))? result: +result.toPrecision(10).replace(/0+$/, "");
     return formattedRes;
+}
+
+const operate = (a, b, operator) => {
+    switch(operator) {
+        case "add": return add(a, b);
+        case "subtract": return subtract(a, b);
+        case "divide": return divde(a, b);
+        case "multiply": return multiply(a, b);
+        case "power": return power(a, b);
+    }
+    return "Error";
 }
